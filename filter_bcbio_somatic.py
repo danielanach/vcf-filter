1cd17e1838f5405b67ad7aaa0c3269e67fd249ed
Daniela Nachmanson <danielanachman@gmail.com>
2019-12-10 10:10:26 -0800

    Adding a requirement to be in a cancer gene if found in population DBs

--- filter_bcbio_somatic.py
+++ filter_bcbio_somatic.py
@@ -1,8 +1,10 @@
 from pysam import VariantFile
+import pandas as pd
+import string
 
 def filter_bcbio_somatic(in_vcf_name,
                          out_vcf_name,
-                         max_maf,
+                         max_maf=0.001,
                          max_af=0.9,
                          loh_df=None):
 
@@ -11,28 +13,54 @@ def filter_bcbio_somatic(in_vcf_name,
     out_vcf = open(out_vcf_name,mode='w')
     out_vcf.write(vcf_in.header.__str__())
 
+    df = pd.read_csv('/Users/d/work/dcis_lcm_exome/data/cancer_genes.tsv',sep='\t')
+    cancer_gene_list = [g.strip() for g in df['Gene Symbol']]
+
     for rec in vcf_in:
 
+        write_var = True
         if 'max_aaf_all' in rec.info.keys():
             # Found in population databases
-
             if is_frequent(rec,max_maf):
 
-                if is_in_clinvar(rec) and not is_benign_in_clinvar(rec):
-                    out_vcf.write(rec.__str__())
+                if not in_cancer_gene(rec,cancer_gene_list):
                     continue
 
-                if is_cosmic_variant(rec,3):
-                    out_vcf.write(rec.__str__())
+                if not is_in_clinvar(rec) and not is_cosmic_variant(rec):
                     continue
 
+                if is_in_clinvar(rec) and is_benign_in_clinvar(rec):
+                    continue
+
+                if is_clonal_and_not_loh(rec,max_af,loh_df):
+                    continue
                 else:
-                    continue
-
-        if is_clonal_and_not_loh(rec,max_af,loh_df):
-                continue
+                    out_vcf.write(rec.__str__())
         else:
-            out_vcf.write(rec.__str__())
+            if is_clonal_and_not_loh(rec,max_af,loh_df):
+                continue
+            else:
+                try:
+                    out_vcf.write(rec.__str__())
+                except:
+                    s = rec.info['dbNSFP_clinvar_trait']
+                    printable = set(string.printable)
+                    filter(lambda x: x in printable, s)
+                    rec.info['dbNSFP_clinvar_trait'] = ''.join(filter(lambda x: x in printable, s))
+                    out_vcf.write(rec.__str__())
+
+def in_cancer_gene(rec,cancer_gene_list):
+    found_t = False
+    for a in rec.info.get('ANN'):
+        if a.split('|')[5].strip() == 'transcript':
+            found_t = True
+            ann = a
+    if not found_t:
+        ann = rec.info.get('ANN')[0]
+    ann = ann.split('|')
+    gene = ann[3]
+
+    return gene in cancer_gene_list
 
 def is_clonal_and_not_loh(rec,max_af,loh_df):
 
@@ -70,7 +98,7 @@ def is_frequent(rec,max_maf):
     return frequent
 
 def is_in_clinvar(rec):
-    if 'dbNSFP_clinvar_clnsig' in rec.info.keys():
+    if ('clinvar_sig' in rec.info.keys()) or ('dbNSFP_clinvar_clnsig' in rec.info.keys()):
         return True
     else:
         return False
@@ -78,18 +106,22 @@ def is_in_clinvar(rec):
 def is_benign_in_clinvar(rec):
 
     benign = False
-
-    if '2' in rec.info['dbNSFP_clinvar_clnsig'] or '3' in rec.info['dbNSFP_clinvar_clnsig']:
-        benign = True
+    if 'clinvar_sig' in rec.info.keys():
+        sig = ''.join(rec.info['clinvar_sig']).lower()
+        if 'benign' in sig:
+            benign = True
+    if 'dbNSFP_clinvar_clnsig' in rec.info.keys():
+        if ('2' in ''.join(rec.info['dbNSFP_clinvar_clnsig'])) or ('3' in ''.join(rec.info['dbNSFP_clinvar_clnsig'])):
+            benign = True
 
     return benign
 
-def is_cosmic_variant(rec,min_cnt):
+def is_cosmic_variant(rec):
 
     cosmic = False
 
-    if 'dbNSFP_COSMIC_CNT' in rec.info.keys():
-        for c in rec.info['dbNSFP_COSMIC_CNT']:
-            if c >= min_cnt:
-                cosmic = True
+    if 'cosmic_ids' in rec.info.keys():
+        if not 'COSN' in "".join(rec.info['cosmic_ids']):
+            cosmic = True
+
     return cosmic
